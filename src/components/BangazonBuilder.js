7c0a4ba48dc02a3e40ef0fee69b5b91faadfaa89
leigharobinson <lmarobinson24@gmail.com>
2020-09-02 11:25:28 -0500

    Added infomraiton to Auth

---
+++ src/components/BangazonBuilder.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+function BangazonBuilder() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Bangazon</p>
+        <p>An UpperClassTwit Production</p>
+      </header>
+    </div>
+  );
+}
+
+export default BangazonBuilder;
