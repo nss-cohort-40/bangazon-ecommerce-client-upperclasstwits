b54c97cc69347d00d9b5a9fca9b6bc78c98bb4af
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-09-02 11:39:41 -0500

    building nav

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BangazonBuilder />
   </React.StrictMode>,
   document.getElementById('root')
 );
