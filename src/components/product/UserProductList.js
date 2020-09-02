35e6b725bd033e9b7d869f43a82a1542954337e4
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-09-02 15:04:45 -0500

    starting user product list

---
+++ src/components/product/UserProductList.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useRef } from "react"
+
+const UserProducts = props => {
+    const [userProductsList, setUserProductsList] = useState([])
+
+    const getUserProducts = () => {
+
+        fetch("http://localhost:8000/products?customer_id")
+    }
+}
