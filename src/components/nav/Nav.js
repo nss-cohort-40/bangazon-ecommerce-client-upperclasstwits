54a96590340d857481fcd8fb9b8749ced319facc
leigharobinson <lmarobinson24@gmail.com>
2020-09-04 13:02:03 -0500

    one more merge with master to fix left out resolution

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,10 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css"
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
-<<<<<<< HEAD
-=======
 import "./Nav.css";
->>>>>>> master
 
 const NavBar = (props) => {
   const { isAuthenticated, logout } = useSimpleAuth();
